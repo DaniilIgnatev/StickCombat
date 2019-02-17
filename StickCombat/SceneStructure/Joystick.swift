070efed13689285bd5f6fbf5200d63e8f791b695
Daniel <i.daniil2010@yandex.ru>
2019-02-17 18:22:41 +0400

    crashFix

--- StickCombat/SceneStructure/Joystick.swift
+++ StickCombat/SceneStructure/Joystick.swift
@@ -54,6 +54,6 @@ class HorizontalJoystick: SKNode, Joystick {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
 }
